0a912a3efd53d74ce18fb4038f727eaa773dded7
Manas <manas94gupta@gmail.com>
2017-03-31 17:57:23 +0530

    change scroll speed based on input value

--- pageScroll.js
+++ pageScroll.js
@@ -1,7 +1,7 @@
-console.log(speed);
-pageScroll();
+pageScroll(speed);
 
 function pageScroll() {
+    console.log(speed);
     window.scrollBy(0,1);
-    scrolldelay = setTimeout(pageScroll,30);
+    scrolldelay = setTimeout(pageScroll,speed);
 }
